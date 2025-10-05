5a503e8e34e8c06d865b149e66a52cc68cee8c57
Linc <762699299@qq.com>
2025-10-05 22:48:27 +0800

    chore: hide pin btn in no debug mode

--- src/Tauri/src/main.ts
+++ src/Tauri/src/main.ts
@@ -58,8 +58,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
 // 注意api/window里的功能很多都需要开启权限，否则控制台会报错告诉你应该开启哪个权限
 import { getCurrentWindow } from '@tauri-apps/api/window'
-import { initMenu } from './contextmenu';
-// import { global_setting } from '../../Core/Setting';
+import { initMenu } from './contextmenu'
+import { global_setting } from '../../Core/Setting'
 
 // 前端模块
 window.addEventListener("DOMContentLoaded", () => {
@@ -84,7 +84,8 @@ window.addEventListener("DOMContentLoaded", () => {
     console.log('emit paste-event')
   }*/
 
-  // 置顶按钮
+  // 置顶按钮 (仅 debug 时显示)
+  if (!global_setting.isDebug) return;
   const pin_btn = document.createElement('button'); main.appendChild(pin_btn); pin_btn.classList.add('btn-1');
   // https://lucide.dev/icons/pin
   pin_btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pin-icon lucide-pin"><path d="M12 17v5"/><path d="M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H8a2 2 0 0 0 0 4 1 1 0 0 1 1 1z"/></svg>`
