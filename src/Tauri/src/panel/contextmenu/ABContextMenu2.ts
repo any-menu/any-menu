01aba3cb53f4c1561d0c9b88af8f480aae592c39
Linc <762699299@qq.com>
2025-10-19 01:44:00 +0800

    refactor(Tauri): move contextmenu to panel

---
+++ src/Tauri/src/panel/contextmenu/ABContextMenu2.ts
@@ -0,0 +1,8 @@
+import { ABContextMenu } from "../../../../Core/panel/contextmenu/index"
+
+export class ABContextMenu2 extends ABContextMenu {
+  hide(): void {
+    super.hide()
+    // hideWindow()
+  }
+}
