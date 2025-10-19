af3993d3733c9fa56c036d4eb55954b9fbcba2ef
Linc <762699299@qq.com>
2025-10-19 21:55:46 +0800

    feat(store): new plugin: md-title-linter2

--- store/dict/md_title_linter.js
+++ store/dict/md_title_linter.js
@@ -27,32 +27,31 @@
 const plugin = {
     metadata: {
         id: 'anymenu-md-title-linter',
-        name: '清除md中的标题加粗和链接',
+        name: '清除md标题样式冗余',
         version: '1.0.0',
         author: 'LincZero'
     },
 
     async process(str) {
-		if (!str) {
+        if (!str) {
             console.warn('需要选中文本后再执行');
             return;
         }
-		
-		const lines = str.split('\n');
-		for (let i = 0; i < lines.length; i++) {
-			let line = lines[i]
+        
+        const lines = str.split('\n');
+        for (let i = 0; i < lines.length; i++) {
+            // let line = lines[i]
 
-			// 是否标题
-			const headingMatch = line.match(/^(#{1,6})\s*(.*)$/)
-			if (!headingMatch) continue
+            // 是否标题
+            const headingMatch = lines[i].match(/^(#{1,6})\s*(.*)$/)
+            if (!headingMatch) continue
+            
+            lines[i] = lines[i].replace(/\[([^\]]+)\]\([^)]+\)/g, '$1') // 去除链接
+            lines[i] = lines[i].replace(/(\*\*|__)(.*?)\1/g, '$2')      // 先去除加粗
+            lines[i] = lines[i].replace(/(\*|_)(.*?)\1/g, '$2')           // 再去除斜体
+        }
+        str = lines.join('\n')
 
-			
-			line = line.replace(/\[([^\]]+)\]\([^)]+\)/g, '$1') // 去除链接
-			line = line.replace(/(\*\*|__)(.*?)\1/g, '$2')      // 先去除加粗
-			line = line.replace(/(\*|_)(.*?)\1/g, '$2')   	    // 再去除斜体
-		}
-		str = lines.join('\n')
-
-		return str
+        return str
     }
 }
