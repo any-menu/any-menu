0ae14d978a6ce862271a60af91da65affaece92f
Linc <762699299@qq.com>
2025-10-20 07:54:03 +0800

    enhance(Tauri): adShortcut: arrow key

--- docs/zh/adShoftcut/README.md
+++ docs/zh/adShoftcut/README.md
@@ -5,3 +5,8 @@
 其方案原自我用了很久的一个方案: https://github.com/LincZero/LincZero-Help-Oneself/tree/main/Kanata/InstallerAndScript
 
 具体的教程和文档也见上面的链接
+
+此外，这里在上面的方案基础上，还添加了打开 AnyMenu 的快捷键:
+
+- `Caps+M`: Open any **Menu**
+- `Caps+N`: Open mini **Note**
--- src/Tauri/src-tauri/src/ad_shortcut.rs
+++ src/Tauri/src-tauri/src/ad_shortcut.rs
@@ -36,24 +36,35 @@ pub fn _init_ad_shortcut() {
     }).unwrap();
 }
 
-/** 可以拦截原行为，会阻塞 */
+/** 可以拦截原行为，会阻塞
+ * 
+ * 一些机制: KeyPress 会在长按时一直触发, 直到按下下一个键, 上一个键就不再一直触发 (哪怕还按着)
+ */
 pub fn init_ad_shortcut(app_handle: tauri::AppHandle) {
     let caps_active = Cell::new(false);         // 是否激活 Caps 层
     let caps_active_used = Cell::new(false);    // 是否使用过激活后的 Caps 层
-    let skip_next_caps_event = Cell::new(false);// 跳过一次原 Caps 行为
+    let virtual_event_flag = Cell::new(false);  // 跳过虚拟行为，避免递归
 
     // 注意: 这是一个 Fn (非FnMut/FnOnce) 闭包，表示可以多次且并发调用
     // 所以这里的 caps_active 要改成 Cell 类型以确保并发安全
     let callback = move |event: Event| -> Option<Event> {
+        // 模拟按键 - 语法糖
+        let simu = |event_type: &EventType| {
+            virtual_event_flag.set(true);
+            let _ = simulate(event_type);
+            virtual_event_flag.set(false);
+            // let delay = time::Duration::from_millis(20);
+            // thread::sleep(delay);
+        };
+
+        // 如果是按下键则打印 (debug)
+        // if let EventType::KeyPress(key) = event.event_type {
+        //     println!("KeyDown {:?}", key);
+        // }
+
         // 避免捕获自身模拟的虚拟按键
-        if skip_next_caps_event.get() {
-            if event.event_type == EventType::KeyRelease(Key::CapsLock) {
-                return Some(event)
-            }
-            if event.event_type == EventType::KeyPress(Key::CapsLock) {
-                skip_next_caps_event.set(false);
-                return Some(event)
-            }
+        if virtual_event_flag.get() {
+            return Some(event)
         }
 
         // CapsLock 状态
@@ -65,38 +76,44 @@ pub fn init_ad_shortcut(app_handle: tauri::AppHandle) {
         if event.event_type == EventType::KeyRelease(Key::CapsLock) {
             caps_active.set(false);
             if !caps_active_used.get() { // 没用过，则正常大小写切换
-                skip_next_caps_event.set(true);
+                virtual_event_flag.set(true);
                 let _ = simulate(&EventType::KeyRelease(Key::CapsLock));
                 thread::sleep(time::Duration::from_millis(10));
                 let _ = simulate(&EventType::KeyPress(Key::CapsLock));
-                thread::sleep(time::Duration::from_millis(10));
+                virtual_event_flag.set(false);
             }
             return Some(event) // 禁用 CapsLock 键
         }
 
-        // Caps+N
+        // Caps+*
         if caps_active.get() {
             caps_active_used.set(true);
             // Caps+Esc, 伪造 CapsLock 按下和释放事件，来切换大小写
             // 不知道为什么，+Esc的识别总是不够灵敏
             if event.event_type == EventType::KeyRelease(Key::Escape) {
-                skip_next_caps_event.set(true);
-                let _ = simulate(&EventType::KeyRelease(Key::CapsLock));
+                virtual_event_flag.set(true);
+                simu(&EventType::KeyRelease(Key::CapsLock));
                 thread::sleep(time::Duration::from_millis(10));
-                let _ = simulate(&EventType::KeyPress(Key::CapsLock));
+                simu(&EventType::KeyPress(Key::CapsLock));
 
                 return None
             }
+            if event.event_type == EventType::KeyPress(Key::KeyU) { simu(&EventType::KeyPress(Key::UpArrow)); return None }
+            if event.event_type == EventType::KeyPress(Key::KeyJ) { simu(&EventType::KeyPress(Key::LeftArrow)); return None }
+            if event.event_type == EventType::KeyPress(Key::KeyK) { simu(&EventType::KeyPress(Key::DownArrow)); return None }
+            if event.event_type == EventType::KeyPress(Key::KeyL) { simu(&EventType::KeyPress(Key::RightArrow)); return None }
             if event.event_type == EventType::KeyPress(Key::KeyF) {
                 println!("Caps+F detected!");
-                // 这里可以用 tauri::api::process::Command 或 tauri::window.emit 通知前端
                 return None
             }
             if event.event_type == EventType::KeyPress(Key::KeyN) || event.event_type == EventType::KeyPress(Key::KeyM) {
                 println!("Caps+N/M detected!");
+                // 有bug: 这里会通知前端，召唤出窗口。但窗口召唤后这里的按键监听会失效，并且鼠标无法移动，疑似卡死
+                // 但可以按 Esc 退出窗口，并再单击一下 Caps 键。能恢复正常
                 app_handle.emit("active-window-toggle", ()).unwrap();
                 return None
             }
+            println!("ignore Caps+* {:?}", event.event_type);
             return None
         }
 
@@ -106,3 +123,9 @@ pub fn init_ad_shortcut(app_handle: tauri::AppHandle) {
         println!("Error: {:?}", error)
     }
 }
+
+// fn simu(event_type: &EventType) {
+//     let _ = simulate(event_type);
+//     // let delay = time::Duration::from_millis(20);
+//     // thread::sleep(delay);
+// }
