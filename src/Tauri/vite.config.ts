350e5f53ae015a2426021b0184906ceaffe99181
Linc <762699299@qq.com>
2025-10-22 14:10:38 +0800

    fix: app config page error
    没有打包config.html导致config页面空白

--- src/Tauri/vite.config.ts
+++ src/Tauri/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -25,6 +26,15 @@ export default defineConfig(async () => ({
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), // 主页入口
+        config: resolve(__dirname, 'config.html'),  // 配置页入口
+      },
     },
   },
 }));
