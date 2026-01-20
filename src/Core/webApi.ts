01d41f4e72f9afb164967e07dca7888611a038a7
Linc <762699299@qq.com>
2026-01-20 17:17:57 +0800

    enhance: gitee api code optimize

--- src/Core/webApi.ts
+++ src/Core/webApi.ts
@@ -1,4 +1,6 @@
 /**
+ * gitee/github api
+ * 
  * take from: https://github.com/Obsidian-Forge/obsidian-i18n/
  * 然后我去除了obsidian依赖
  * 
@@ -8,31 +10,34 @@
 import { global_setting } from './setting'
 
 /**
- * gitee api (二次封装 base api (网络与本地文件读写))
+ * gitee/github api (二次封装 base api (网络与本地文件读写))
  * 
  * 与后端交互的 API，此处使用了较为节约成本的 gitee 作为存储和交互的服务器
  * 暂时用不上的放后面，并进行了一些分类
+ * 
+ * 注意: apiUrl 或加 token，通常有更高的速度 (1000次/h) 和访问次数，普通 url 则很容易出现 403
  */
 export class API {
   giteeOwner = 'any-menu';
   giteeRepo = 'any-menu';
   giteeBranch = 'main';
-  // apiUrl / 加 token 通常有更高的速度 (1000次/h)，普通 url 则很容易出现 403
+
+  // gitee 相关
   giteeBaseUrl = `https://gitee.com/${this.giteeOwner}/${this.giteeRepo}/raw/${this.giteeBranch}/`; // raw是原文本，blob是网页
   giteeBlobUrl = `https://gitee.com/${this.giteeOwner}/${this.giteeRepo}/blob/${this.giteeBranch}/`; // raw是原文本，blob是网页
   giteeApiUrl = `https://gitee.com/api/v5/repos/${this.giteeOwner}/${this.giteeRepo}/`;
   // 后面的子api一般有: contents issue collaborators releases raw 等，见: https://gitee.com/api/v5/swagger#/getV5ReposOwnerRepoRawPath
-  // https://gitee.com/api/v5/repos/{owner}/{repo}/raw/{path}
-  
+  // https://gitee.com/api/v5/repos/{owner}/{repo}/raw/{path}  
   giteeToken: string|null = '6ca4bf01f660cc1b4fdc98f14aaff4f9'; // onlyReadApi x
+
+  // 弃用
   // path = `store/dict/`
   // giteeLanguage = 'zh-cn';
   // I18N_ADMIN_TOKEN = '';
 
-  constructor() {
-  }
+  constructor() {}
 
-  // 获取网络目录 (有那些词典可以下载)
+  /// 获取网络目录 (有那些词典可以下载)
   public async giteeGetDirectory() {
     return await global_setting.api.urlRequest({
       url: `${this.giteeBaseUrl}store/directory/dir.json`,
@@ -44,7 +49,7 @@ export class API {
     });
   }
 
-  // 获取网络文件内容
+  /// 获取网络文件内容
   public async giteeGetDict(relPath: string) {
     return await global_setting.api.urlRequest({
       url: `${this.giteeBaseUrl}store/dict/${relPath}`,
@@ -56,7 +61,7 @@ export class API {
     });
   }
 
-  // 获取网络目录 (有那些词典可以下载)
+  /// 获取网络目录 (有那些词典可以下载)
   public async giteeGetDirectory3() {
     const res = await global_setting.api.urlRequest({
       url: `${this.giteeApiUrl}contents/store/directory/dir.json?ref=${this.giteeBranch}`,
@@ -85,7 +90,7 @@ export class API {
     return res;
   }
 
-  // 获取网络文件内容
+  /// 获取网络文件内容
   public async giteeGetDict3(relPath: string) {
     const res = await global_setting.api.urlRequest({
       url: `${this.giteeApiUrl}contents/store/dict/${relPath}?ref=${this.giteeBranch}`,
@@ -113,7 +118,7 @@ export class API {
     return res;
   }
 
-  // 获取本地目录 (已经下载了哪些词典)
+  /// 获取本地目录 (已经下载了哪些词典)
   public async localGetDirectory() {
     const ret: string[] = await global_setting.api.readFolder(global_setting.config.dict_paths)
     return ret
