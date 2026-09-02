5e13d621e6c8bc4668ed66c3d9ecb55034875338
Linc <762699299@qq.com>
2026-09-02 21:38:58 +0800

    chore(browser): move to showEvent.ts

--- src/Browser/src/main.ts
+++ src/Browser/src/main.ts
@@ -11,12 +11,13 @@ import { exampleSetup } from "prosemirror-example-setup"
 import { selectionPlugin } from "./ProseMirror/plugin"
 
 import { global_setting } from '@/Core/shared/setting'
-import { activeAMPanel, AMPanel } from '@/Core/panels/MulPanel'
+import { AMPanel } from '@/Core/panels/MulPanel'
 import { initSettingTab_1, initSettingTab_2 } from '@/Core/modules/settingPanel/SettingTab'
 import { initMenuData } from '@/Core/initTool'
 import { DocumentListeners } from '@/Core/modules/editor/event'
 import { EditorTools } from '@/Core/modules/editor/cursor'
 import { initApi, initApi_with_server } from './utils/initApi'
+import { addShowPanelBtn } from "./modules/showEvent"
 
 // #region 启动时阅读配置文件
 
@@ -45,26 +46,7 @@ window.addEventListener("DOMContentLoaded", async () => {
       btn.innerText = 'Test, show panel'
       btn.classList.add('am-browser-debug-btn')
 
-      // 非失焦的按钮行为
-      btn.addEventListener('mousedown', (e) => {
-        e.preventDefault(); // 阻止默认行为（阻止按钮获取焦点）
-        e.stopPropagation();
-        // 手动创建一个 click 事件并立即派发 (触发click但不触发焦点改变)
-        btn.dispatchEvent(new MouseEvent('click', {
-          bubbles: true,
-          cancelable: true,
-          view: window
-        }));
-      });
-      btn.onclick = () => {
-        if (!activeAMPanel) return
-        activeAMPanel.panel_hide()
-        activeAMPanel.panel_show(
-          {x: 30, y: 200, is_reverse: false},
-          [...global_setting.config.panel_preset2[0].list, 'debug'],
-          true
-        )
-      }
+      addShowPanelBtn(btn)
   }
 
   // 文本框集
